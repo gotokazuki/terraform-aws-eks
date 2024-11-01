459e6bf475eaa78699e4aa80777dcab2de72e6ca
gotokazuki <gotokazuki@pm.me>
2024-11-01 11:02:39 +0900

    add arm64 example

---
+++ examples/arm64/locals.tf
@@ -0,0 +1,26 @@
+locals {
+  prefix                    = "test-dev"
+  ipv4_cidr                 = "10.0.0.0/16"
+  ipv4_cidr_newbits         = 3
+  subnets_number            = 2
+  eks_cluster_name          = "example"
+  eks_cluster_version       = "1.30"
+  eks_managed_node_ami_type = "arm64"
+  eks_managed_node_instance_types = [
+    "t4g.medium",
+  ]
+  k8s_aws_auth_map_users = [
+    {
+      iam_user = "arn:aws:iam::xxxxxxxxxx:user/username"
+      username = "username"
+      groups = [
+        "system:masters",
+      ]
+    }
+  ]
+  default_tags = {
+    owner      = "ownername"
+    repository = "terraform-aws-eks"
+    created_by = "terraform"
+  }
+}
