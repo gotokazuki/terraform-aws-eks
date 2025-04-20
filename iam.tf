f2fee4bd478fb54f58e11e98895b56f9c3f7fc8f
gotokazuki <gotokazuki@pm.me>
2025-04-20 12:18:41 +0900

    remove unused iam policy for eks node

--- iam.tf
+++ iam.tf
@@ -56,7 +56,6 @@ resource "aws_iam_role_policy_attachment" "eks_node" {
   for_each = toset([
     "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
     "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
-    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
     "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
     "arn:aws:iam::aws:policy/AutoScalingFullAccess",
     "arn:aws:iam::aws:policy/AmazonEKSWorkerNodeMinimalPolicy",
